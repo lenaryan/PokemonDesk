ea00d44fc4487d14254b20ffeb39026d61f77f5f
Lena Ryan <deggmb@yandex.ru>
2020-11-08 01:18:47 +0300

    adding layout component + small changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import Layout from '../../components/Layout';
+
+import s from './Home.module.scss';
 
 const Home = () => {
   return (
     <>
       <Header />
-      <main>
-        <div className="container">PIKA-PIKA</div>
-      </main>
+      <Layout className={s.contentWrap}>
+        <div className={s.contentText}>
+          <h1>
+            <b>Find</b> all your favourite <b>Pokemon</b>
+          </h1>
+        </div>
+        <div className={s.contentParallax}>Parallax div</div>
+      </Layout>
       <Footer />
     </>
   );
