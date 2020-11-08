da546085b39a65d24e5fac936178949265848787
Lena Ryan <deggmb@yandex.ru>
2020-11-08 20:08:54 +0300

    added heading component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,7 @@ import Footer from '../../components/Footer';
 import Layout from '../../components/Layout';
 import Button from '../../components/Button';
 import Parallax from '../../components/Parallax';
+import Heading from '../../components/Heading';
 
 import s from './Home.module.scss';
 
@@ -11,9 +12,9 @@ const Home = () => {
     <>
       <Layout className={s.contentWrap}>
         <div className={s.contentText}>
-          <h1>
+          <Heading level="1">
             <b>Find</b> all your favourite <b>Pokemon</b>
-          </h1>
+          </Heading>
           <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
           <Button onClick={() => {}}>See pokemons</Button>
         </div>
--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from '../../components/Footer';
 import Layout from '../../components/Layout';
+import Heading from '../../components/Heading';
 
 const Pokedex = () => {
   return (
     <>
       <Layout>
-        <h1>Pokedex</h1>
+        <Heading level="1">Pokedex H1</Heading>
+        <Heading level="2">Pokedex H2</Heading>
       </Layout>
       <Footer />
     </>
