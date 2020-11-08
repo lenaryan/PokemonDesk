aa1c48a69d99aa6516d67ab401054a461de7dbc6
Lena Ryan <deggmb@yandex.ru>
2020-11-08 21:44:18 +0300

    editing button component

--- src/pages/Pokedex/index.tsx
+++ src/pages/Pokedex/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Footer from '../../components/Footer';
 import Layout from '../../components/Layout';
 import Heading from '../../components/Heading';
+import Button from '../../components/Button';
+
+// import { Colors, Sizes } from '../../components/Button'
 
 const Pokedex = () => {
   return (
@@ -9,6 +12,25 @@ const Pokedex = () => {
       <Layout>
         <Heading level="1">Pokedex H1</Heading>
         <Heading level="2">Pokedex H2</Heading>
+        <Heading level="3">Pokedex H3</Heading>
+        <Heading level="4">Pokedex H4</Heading>
+        <Heading level="5">Pokedex H5</Heading>
+        <Heading level="6">Pokedex H6</Heading>
+        <Button fullWidth onClick={() => {}}>
+          Button Full Width
+        </Button>
+        <Button size="small" onClick={() => {}}>
+          Button Small
+        </Button>
+        <Button color="primary" onClick={() => {}}>
+          Button Primary
+        </Button>
+        <Button color="primary" size="small" onClick={() => {}}>
+          Button Primary Small
+        </Button>
+        <Button color="primary" fullWidth onClick={() => {}}>
+          Button Primary Full Width
+        </Button>
       </Layout>
       <Footer />
     </>
